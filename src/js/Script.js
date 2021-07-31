58c0f8dd211ce384fd959518c81d34d485551a73
Xlomin <85639629+Xlomin@users.noreply.github.com>
2021-07-31 21:31:09 +0300

    Add files via upload

--- src/js/Script.js
+++ src/js/Script.js
@@ -1,38 +1,56 @@
-"use strict"
+$(document).ready(function () {
+  "use strict"
 
-let siteType = prompt(`Выберите тип сайта:
-\n 1 - сайт визитка
-\n 2 - интернет магазин
-\n 3 - корпоративный сайт`);
+  $(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
 
-let design = prompt(`Выберите дизайн:
-\n 1 - минималистичный
-\n 2 - шаблонный
-\n 3 - уникальный`);
+    $(".section").each((index, element) => {
 
-let adaptability = prompt(`Нужна ли адаптинвость:
-\n 1 - да
-\n 2 - нет`);
+      if ($(element).offset().top - $("nav").outerHeight() <= scrollDistance) {
+        $("nav a").each((index, element) => {
+          if ($(element).hasClass("active")) {
+            $(element).removeClass("active");
+          }
+        });
+        $("nav li:eq" + index + ")").find("a").addClass("active");
+      }
+    });
+  });
 
-let resultPrice = 0;
-let term = 0;
+  /*  let siteType = prompt(`Выберите тип сайта:
+  \n 1 - сайт визитка
+  \n 2 - интернет магазин
+  \n 3 - корпоративный сайт`);
+  
+    let design = prompt(`Выберите дизайн:
+  \n 1 - минималистичный
+  \n 2 - шаблонный
+  \n 3 - уникальный`);
+  
+    let adaptability = prompt(`Нужна ли адаптинвость:
+  \n 1 - да
+  \n 2 - нет`);*/
 
-function calculate(siteType, design, adaptability) {
-  resultPrice = 1000 * siteType;
-  if (design == 1) {
-    resultPrice = resultPrice * 1.2;
-  } else if (design == 2) {
-    resultPrice = resultPrice * 1.6;
-  } else if (design == 3) {
-    resultPrice = resultPrice * 2;
+  let resultPrice = 0;
+  let term = 0;
+
+  function calculate(siteType, design, adaptability) {
+    resultPrice = 1000 * siteType;
+    if (design == 1) {
+      resultPrice = resultPrice * 1.2;
+    } else if (design == 2) {
+      resultPrice = resultPrice * 1.6;
+    } else if (design == 3) {
+      resultPrice = resultPrice * 2;
+    }
+    if (adaptability == 1) {
+      resultPrice = resultPrice * 1.5;
+    }
+    term = Math.round(resultPrice / 250);
   }
-  if (adaptability == 1) {
-    resultPrice = resultPrice * 1.5;
-  }
-  term = Math.round(resultPrice / 250);
-}
 
-calculate(siteType, design, adaptability);
+  calculate(siteType, design, adaptability);
 
-alert(`Примерная стоимость: ~${resultPrice} ₽
-\nПримерные сроки: ~${term} дней`);
+  /*alert(`Примерная стоимость: ~${resultPrice} ₽
+\nПримерные сроки: ~${term} дней`);*/
+});
